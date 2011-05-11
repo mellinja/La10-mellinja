8413a169e337f8f098e03f136b161164c342d264
Jackson Melling <mellinja@rose-hulman.edu>
2011-05-10 20:40:41 -0400

    task 5 completed

---
+++ Fortune-Team12/features/step_definitions/fortune_steps.rb
@@ -0,0 +1,6 @@
+Then /^I should see a fortune$/ do
+fortune = page.find(".fortune")
+fortune.text.should match /^\n.*\n\s*$/
+end
+
+require 'fortunedata'
--- Fortune-Team12/fortune.rb
+++ Fortune-Team12/fortune.rb
@@ -2,5 +2,13 @@ require "rubygems"
 require 'sinatra/base'
 require 'haml'
 require 'thin'
-class Fortune < Sinatra::Base
+
+
+class Fortune < Sinatra::Base	
+	get '/fortune/random/?' do
+		@fortune = $fortunes[rand($fortunes.size)]
+		haml :fortune
+	end
 end
+
+require 'fortunedata'
